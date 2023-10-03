6c0426df4dade18394cfbba5d9bb5f5c291f32c6
M3Z1M <nizilplay2004@gmail.com>
2023-10-03 22:22:15 +0300

    Added button.cpp and some code in it.

---
+++ button.cpp
@@ -0,0 +1,43 @@
+#include "button.h"
+
+void Button::draw()
+{
+    DWORD drawed_text;
+    SetConsoleTextAttribute(cmd_handle, 7);
+    if(selected)
+    {
+        SetConsoleTextAttribute(cmd_handle, select_color);
+    }
+
+    for (int litter = 0; litter < 20; litter++)
+    {
+        COORD litter_pos;
+        char cur_lit;
+        cur_lit = text[litter];
+        litter_pos.X = position.X + litter;
+        litter_pos.Y = position.Y;
+        FillConsoleOutputCharacter(cmd_handle, cur_lit, 1, litter_pos, &drawed_text);
+    }
+}
+
+void Button::remove()
+{
+    COORD pos;
+    DWORD drawed_text;
+    for(int x = 0; x < 20; x++)
+    {
+        pos.X = position.X + x;
+        pos.Y = position.Y;
+        FillConsoleOutputCharacter(cmd_handle, ' ', 1, pos, &drawed_text);
+    }
+}
+
+void Button::select()
+{
+    selected = true;
+}
+
+void Button::press()
+{
+    selected = false;
+}
--- button.h
+++ button.h
@@ -2,9 +2,14 @@
 
 struct Button
 {
-    int width, height;
+    HANDLE cmd_handle = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD position;
+    short select_color = 6;
+    int width, height;
+    bool selected;
     char text[20];
+    void draw();
+    void remove();
     void select();
     void press();
 };
